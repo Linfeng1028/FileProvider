336a8dea84748d31053de0e79a87adb1d7ba4469
linfeng.wang <linfeng.wang@juphoon.com.cn>
2019-12-25 19:57:41 +0800

    feature: first commit

---
+++ app/src/main/java/com/mydomain/MainActivity.java
@@ -0,0 +1,122 @@
+package com.mydomain;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.io.FileNotFoundException;
+
+import static com.mydomain.SelectPictureUtils.REQUEST_CODE_CAPTURE_IMAGE;
+import static com.mydomain.SelectPictureUtils.REQUEST_CODE_CROP_IMAGE;
+import static com.mydomain.SelectPictureUtils.REQUEST_CODE_PICK_IMAGE;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_PERMISSIONS_STORAGE = 100;
+    private static final int REQUEST_CODE_PERMISSIONS_CAMERA = 101;
+
+    private ImageView mShowImageView;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowImageView = findViewById(R.id.iv_show);
+
+        Log.d("wlfTest", getFilesDir().toString());
+        Log.d("wlfTest", getCacheDir().toString());
+        Log.d("wlfTest", Environment.getExternalStorageDirectory().toString());
+        Log.d("wlfTest", getExternalFilesDir(null).toString());
+        Log.d("wlfTest", getExternalCacheDir().toString());
+        Log.d("wlfTest", getExternalMediaDirs().toString());
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_choose_picture:
+                if (ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                        PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            REQUEST_CODE_PERMISSIONS_STORAGE);
+                } else {
+                    SelectPictureUtils.chooseImage(this);
+                }
+                break;
+            case R.id.btn_camera:
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) !=
+                        PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.CAMERA},
+                            REQUEST_CODE_PERMISSIONS_CAMERA);
+                } else {
+                    SelectPictureUtils.captureImage(this);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS_STORAGE &&
+                grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            SelectPictureUtils.chooseImage(this);
+        }
+        if (requestCode == REQUEST_CODE_PERMISSIONS_CAMERA &&
+                grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            SelectPictureUtils.captureImage(this);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode) {
+            case REQUEST_CODE_PICK_IMAGE:
+                if (data != null && data.getData() != null) {
+                    mShowImageView.setImageBitmap(SelectPictureUtils.getBitmapFromUri(this,
+                            data.getData()));
+                }
+                break;
+            case REQUEST_CODE_CAPTURE_IMAGE:
+                if (resultCode == RESULT_OK) {
+                    SelectPictureUtils.startPhoneZoom(this, SelectPictureUtils.getContentUri());
+                }
+                break;
+            case REQUEST_CODE_CROP_IMAGE:
+                if (resultCode == RESULT_OK) {
+                    Bitmap bp = null;
+                    try {
+                        // 将 uri 转成位图形式
+                        bp = BitmapFactory.decodeStream(getContentResolver().openInputStream(
+                                SelectPictureUtils.getCropImageUri()
+                        ));
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    mShowImageView.setImageBitmap(bp);
+                }
+                break;
+            default:
+                break;
+        }
+
+    }
+}
---
+++ app/src/main/java/com/mydomain/SelectPictureUtils.java
@@ -0,0 +1,108 @@
+package com.mydomain;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.IOException;
+
+import static android.provider.MediaStore.Images.Media.getBitmap;
+
+public class SelectPictureUtils {
+    static final int REQUEST_CODE_PICK_IMAGE = 200;
+    static final int REQUEST_CODE_CAPTURE_IMAGE = 201;
+    static final int REQUEST_CODE_CROP_IMAGE = 202;
+
+    // 对应 Manifest.xml 中 provider 标签下的 authorities
+    private static final String FILE_PROVIDER_AUTHORITY = "com.mydomain.fileprovider";
+
+    // 保存拍照存放照片的绝对路径
+    private static Uri mContentUri = null;
+    // 保存图片裁剪后的绝对路径
+    private static Uri mCropImageUri = null;
+
+    /**
+     * 使用系统相册选择图片
+     *
+     * @param activity
+     */
+    static void chooseImage(Activity activity) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        activity.startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE);
+    }
+
+    /**
+     * 使用系统相机拍照
+     *
+     * @param activity
+     */
+    static void captureImage(Activity activity) {
+        // 注意这个路径下的目录需要定义在 Manifest.xml-provider-meta-data 标签中来获得共享权限
+        // 否则会抛出 SecurityException 异常
+        File imagePath = new File(activity.getFilesDir(), "images");
+        File newFile = new File(imagePath, "default_image.jpg");
+        if (!imagePath.exists()) {
+            imagePath.mkdirs();
+        }
+        // 注意这里需要使用 content:// 形式暴露，否则在 7.0 以上系统会抛出 FileUriExposedException 异常
+        mContentUri = FileProvider.getUriForFile(activity, FILE_PROVIDER_AUTHORITY, newFile);
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, mContentUri);
+        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        activity.startActivityForResult(intent, REQUEST_CODE_CAPTURE_IMAGE);
+    }
+
+    /**
+     * 使用系统裁剪
+     *
+     * @param activity
+     * @param uri
+     */
+    static void startPhoneZoom(Activity activity, Uri uri) {
+        File file = new File(activity.getExternalCacheDir(), "images");
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        intent.setDataAndType(uri, "image/*");
+        mCropImageUri = Uri.parse("file://" + file.getAbsolutePath() + "/" + System.currentTimeMillis() + ".jpg");
+        // 这个 outputUri 是要使用 Uri.fromFile(file) 生成的，而不是使用FileProvider.getUriForFile
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, mCropImageUri);
+        activity.startActivityForResult(intent, REQUEST_CODE_CROP_IMAGE);
+    }
+
+    /**
+     * 将 uri 转换成 bitmap
+     *
+     * @param activity
+     * @param uri
+     * @return
+     */
+    static Bitmap getBitmapFromUri(Activity activity, Uri uri) {
+        if (uri == null) {
+            return null;
+        }
+        try {
+            return getBitmap(activity.getContentResolver(), uri);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    static Uri getContentUri() {
+        return mContentUri;
+    }
+
+    public static Uri getCropImageUri() {
+        return mCropImageUri;
+    }
+}
